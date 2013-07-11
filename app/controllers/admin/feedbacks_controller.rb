097d31329328acf5c9f98b41921b4684b16c7a10
pbharadiya <parthbharadiya007@gmail.com>
2013-07-11 16:25:29 +0530

    current user can't delete/edit itself, sorting by feedback created

--- app/controllers/admin/feedbacks_controller.rb
+++ app/controllers/admin/feedbacks_controller.rb
@@ -15,7 +15,7 @@ class Admin::FeedbacksController < AdminController
   end
 
   def sort_direction
-    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "desc"
   end
 
 end
