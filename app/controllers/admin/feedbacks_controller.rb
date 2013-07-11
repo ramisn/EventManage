7a1ffee8388d0fed883df32f999e3f2140023724
pbharadiya <parthbharadiya007@gmail.com>
2013-07-11 16:06:02 +0530

    search by enter key, sorting feedback-table column,all AJAX request

---
+++ app/controllers/admin/feedbacks_controller.rb
@@ -0,0 +1,21 @@
+class Admin::FeedbacksController < AdminController
+  helper_method :sort_column, :sort_direction
+  def index
+    @feedbacks = Feedback.search(params[:search]).order(sort_column + ' ' + sort_direction)
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
+  private
+
+  def sort_column
+    Feedback.column_names.include?(params[:sort]) ? params[:sort] : "created_at"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+  end
+
+end
