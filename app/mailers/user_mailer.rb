28e485f6982ff8bd76e9f6400820b7076263358c
pbharadiya <parthbharadiya007@gmail.com>
2013-07-25 16:31:19 +0530

    subscription mail for future use

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -11,4 +11,9 @@ class UserMailer < ActionMailer::Base
     @url  = edit_password_reset_url(user.reset_password_token)
     mail(:to => user.email, :subject => "Your password has been reset")
   end
+
+  def welcome(user)
+    @user = user
+    mail(:to => user.email, :subject => "Thank you for subscribing!")
+  end
 end
