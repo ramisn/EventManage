7de9731ff6857026beb4a38b5ef4bb10925128e8
pbharadiya <parthbharadiya007@gmail.com>
2013-08-28 19:40:21 +0530

    configuration to upload code to appfog

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ EventManagement::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true  #changed to true for appfog configuration
 
   # Generate digests for assets URLs
   config.assets.digest = true
