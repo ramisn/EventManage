12ca0877b57d9207cd1063b0876e10fc5addb80a
pbharadiya <parthbharadiya007@gmail.com>
2013-10-08 15:55:56 +0530

    display event-name along with match result/fixture

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,37 +5,25 @@ class HomeController < ApplicationController
     @events = Event.all
     @photos = Photo.all
     #Logic-1 :: It is faster than Logic-2 in data load.
-    @matches = Match.all
-    @temp1_results = []
-    @temp2_fixtures = []
-    @matches.collect { |m| @temp1_results << m unless m.result.eql?('nil') }
-    @matches.collect { |m| @temp2_fixtures << m if m.result.eql?('nil') }
+    @matches = Match.order("updated_at DESC")
 
     @match_results = []
     @match_fixtures = []
+    @matches.collect { |m| @match_results << m unless m.result.eql?('nil') }
+    @matches.collect { |m| @match_fixtures << m if m.result.eql?('nil') }
+
     @teams_for_results = []
     @teams_for_fixtures = []
-    @events.each do |event|
-      temp = []
-      @temp1_results.collect {|m| temp << m if m.event_id.eql?(event.id) }
-      @match_results << temp
 
-      temp = []
-      @temp2_fixtures.collect {|m| temp << m if m.event_id.eql?(event.id) }
-      @match_fixtures << temp
+    @match_results.each do |match|
+      @teams_for_results << Team.where("id IN (?)", [match.t1,match.t2])
     end
 
-    @match_results.each do |match_result|
-      match_result.each do |match|
-        @teams_for_results << Team.where("id IN (?)", [match.t1,match.t2])
-      end
+    @match_fixtures.each do |match|
+      @teams_for_fixtures << Team.where("id IN (?)", [match.t1,match.t2])
     end
 
-    @match_fixtures.each do |match_fixture|
-      match_fixture.each do |match|
-        @teams_for_fixtures << Team.where("id IN (?)", [match.t1,match.t2])
-      end
-    end
+
     #End-Of-Logic-1#
 
     #render :text => @match_results and return false
